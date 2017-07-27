5fa2b75093e6b554e6bfda145c337ed19f9c815b
Michael Zabka <zabka.michael@gmail.com>
2017-07-27 20:06:51 +0200

    Add polyfill asyncIterator to exported functions

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ export { default as AsyncIteratorStarted } from './AsyncIteratorStarted';
 export { default as IPromiseAction } from './IPromiseAction';
 export { default as ISaga } from './ISaga';
 export { Action as IAction } from 'redux';
+export function polyfillAsyncIterator() { require('./polyfill/asyncIterator'); }
