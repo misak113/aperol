4a81c705f923b0bad649e55f7d0d72f05c473876
Michael Zabka <zabka.michael@gmail.com>
2021-07-19 14:44:38 +0000

    Make processing of combined sagas totally parallel not even waiting before next actions yielding
    * now, the next actions of single saga are emitted once the saga is done. Not waiting for rest of sagas in combination

--- src/combineSagas.ts
+++ src/combineSagas.ts
@@ -25,7 +25,8 @@ export default function combineSagas<TModel extends ICombinedModel>(
 		{}
 	));
 	const updater = async function* (model: TModel, action: Action) {
-		const values = await Promise.all(sagaKeys.map(async function* (key: string) {
+
+		const invoke = async function* (key: string) {
 			const saga = sagas[key];
 			const iterator = saga.updater(model[key], action);
 			let nextResult;
@@ -36,9 +37,46 @@ export default function combineSagas<TModel extends ICombinedModel>(
 				}
 				nextResult = yield item.value;
 			} while (true);
-		}));
-		for (const item of values) {
-			yield* item;
+		};
+
+		const promises: Promise<any[]>[] = [];
+		const results: any[] = [];
+		const errors: Error[] = [];
+		for (const sagaKey of sagaKeys) {
+			const generator = invoke(sagaKey);
+			const promise = (async function () {
+				const values: any[] = [];
+				let nextResult;
+				do {
+					let item: IteratorResult<IUpdaterYield> = await generator.next(nextResult);
+					if (item.done) {
+						break;
+					}
+					nextResult = item.value;
+					values.push(item.value);
+				} while (true);
+				return values;
+			})();
+			promises.push(promise);
+			promise.then(
+				(value: any) => {
+					results.push(value);
+					promises.splice(promises.indexOf(promise), 1);
+				},
+				(error: Error) => {
+					errors.push(error);
+					promises.splice(promises.indexOf(promise), 1);
+				},
+			);
+		}
+		while (promises.length > 0) {
+			await Promise.race(promises);
+			while (results.length > 0) {
+				yield* results.shift();
+			}
+			while (errors.length > 0) {
+				throw errors.shift();
+			}
 		}
 	};
 	return {
